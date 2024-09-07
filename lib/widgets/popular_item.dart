adf499a0f97700905ad448d610ed7e1876c953ac
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-09-08 00:44:24 +0600

    popular

--- lib/widgets/popular_item.dart
+++ lib/widgets/popular_item.dart
@@ -1,27 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PopularItem extends StatelessWidget {
-  const PopularItem({super.key, required this.title, required this.rating, required this.image});
+  const PopularItem(
+      {super.key,
+      required this.title,
+      required this.rating,
+      required this.image});
 
   final String title;
   final String rating;
   final String image;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         // Navigator.push(context, MaterialPageRoute(builder: (context)=>Placeholder()));
       },
       child: Container(
         width: 200,
         height: 240,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.black,
-          image: DecorationImage(image: AssetImage(image),
-            fit: BoxFit.cover,
-            opacity: 0.9,
-          )
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.black,
+            image: DecorationImage(
+              image: AssetImage(image),
+              fit: BoxFit.cover,
+              opacity: 0.9,
+            )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF4D5652),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/vectors/star_1_x2.svg',
+                            width: 20,
+                            height: 20,
+                          ),
+                          SizedBox(width: 5,),
+                          Text(
+                            rating,
+                            style: GoogleFonts.robotoCondensed(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.redAccent,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 10,)
+          ],
         ),
       ),
     );
